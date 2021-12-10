ba5205dd8256c833deb73734ab35f1f574b8df0f
sz-lin <shiaw790709@gmail.com>
2021-12-10 17:08:49 +0800

    Add new file

--- 13_密碼重試程式練習題.py
+++ 13_密碼重試程式練習題.py
@@ -1,17 +1,16 @@
 #最多輸入3次
 #如果正確 就印出 "登入成功"
 #如果不正確 就印出 "密碼錯誤! 還有_次機會"
-x=1
-
+password = '123456'
+i = 3 #剩餘機會
 while 	True:
-	password = input('請輸入密碼:')
-	if password =='a123456':
+	pwd = input('請輸入密碼:')
+	if pwd ==password:
 		print('登入成功')
-		break	
+		break	#跳出迴圈
 	else:
-		if x < 3:
-			print('密碼錯誤! 還有',3-x,'機會')
-		x=x+1
-		if x == 4:
-			print('密碼錯誤! ')
+		i = i - 1
+		print('密碼錯誤! 還有',i,'機會')
+		if i == 0:
+			print('密碼錯誤!機會用完了 ')
 			break
